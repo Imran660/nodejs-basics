a490e0301bbb9a83f3e443274e8f298919e97ff4
Imran940 <immoshaikh91@gmail.com>
2023-03-24 16:26:26 +0530

    added the code

---
+++ build.js
@@ -0,0 +1,11 @@
+
+
+function demo(){
+console.log("project is building..");
+console.log("wait for a couple of seconds");
+setTimeout(() => {
+  console.log("Project is successfully built.");
+}, 2000);
+}
+
+export const check="checking.."
---
+++ index.js
@@ -0,0 +1,16 @@
+//const websiteScraper=require("website-scraper");
+import websiteScraper from "website-scraper";
+// import "./build.js"
+import { check } from "./build.js";
+console.log(check);
+
+// const options = {
+//   urls: ["https://www.npmjs.com/"],
+//   directory: "./downloadedCode1",
+// };
+
+// // promises is a default feature of js, which is an object that's used to handle the future responses
+// websiteScraper(options)
+//   .then(() => console.log("downloaded.."))
+//   .catch((err) => console.log(err));
+// console.log("hey learners let's learn nodejs basics");
